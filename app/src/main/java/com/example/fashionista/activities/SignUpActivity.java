65168f53db02e5be1f450af25d8c92030faf0f58
NguyenHuynhPhucLam <21520316@gm.uit.edu.vn>
2024-09-13 12:12:36 +0700

    Add color

--- app/src/main/java/com/example/fashionista/activities/SignUpActivity.java
+++ app/src/main/java/com/example/fashionista/activities/SignUpActivity.java
@@ -1,9 +1,6 @@
 package com.example.fashionista.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,26 +10,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.fashionista.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button mainBtn = findViewById(R.id.mainBtn);
-        mainBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
+
     }
 }
